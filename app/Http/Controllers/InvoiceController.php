09ca728076c3e05cb63200e452789ca510c4d5db
rakesh <rakesh@NITL-013988>
2021-03-19 09:22:53 +0530

    empty check

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -6,8 +6,6 @@ use Illuminate\Routing\Controller;
 use App\Models\User;
 use App\Models\Address;
 
-
-
 class InvoiceController extends Controller{
     
     function get_data() {
@@ -16,14 +14,19 @@ class InvoiceController extends Controller{
     }
     function view_data($id) {
         $result= Invoice::where('invoice_no',$id)->get();
-        if(!empty($result)){
+        if(!$result->isEmpty()){
             $result[0]['items']= Items::where('invoice_id',$id)->get();
             $user= User::where('id',$result[0]->user_id)->get();
             $address = Address::where('user_id',$result[0]->user_id)->get();
             $result[0]['user']=$user[0];
             $result[0]['address']=$address[0];
-            Invoice::where('invoice_no',$id)->update(['status' =>"sucess"]);
+            if(Invoice::where('invoice_no',$id)->update(['status' =>"success"])){
+                $result[0]['status']="success";
+            }
+            $result=$result[0];
+        }else{
+            $result=array();
         }
-        return view('invoiceView',['invoice'=>$result[0]]);
+        return view('invoiceView',['invoice'=>$result]);
     }
 }
